6370668eb1915fa9cba2df557a4e8d43f403efd9
Tommy CHONG <tommy.chong@ha.org.hk>
2019-07-05 16:28:18 +0800

    use Formik

--- src/specimen/Specimen.tsx
+++ src/specimen/Specimen.tsx
@@ -4,34 +4,17 @@ import {
   CardContent,
   FormLabel,
   Grid,
+  Paper,
   Tab,
-  Tabs,
-  TextField
+  Tabs
 } from "@material-ui/core";
-import AppBar from "@material-ui/core/AppBar";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { Field, Form, Formik } from "formik";
+import { TextField } from "formik-material-ui";
 import React, { useState } from "react";
 
-function MadeWithLove() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Built with love by the "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Material-UI
-      </Link>
-      {" team."}
-    </Typography>
-  );
-}
-
 const useStyles = makeStyles(theme => ({
-  appBar: {
-    position: "relative"
-  },
   layout: {
     width: "auto",
     marginLeft: theme.spacing(2),
@@ -65,144 +48,186 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function useFormInput(initialValue = "") {
-  const [value, setValue] = useState(initialValue);
-
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setValue(event.target.value);
-  }
-  return { value, onChange: handleChange };
-}
-
 export default function Specimen() {
   const classes = useStyles();
 
   const [tabValue, setTabValue] = useState(0);
 
   return (
-    <React.Fragment>
-      <AppBar position="absolute" color="default" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Company name
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Card>
-            <CardContent>
-              <Typography>Patient Information</Typography>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={3}>
-                  <TextField
-                    label="Case"
-                    {...useFormInput("MH1238474589634")}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                  <TextField label="HKID" {...useFormInput("A123456(3)")} />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                  <TextField label="Name" {...useFormInput("DAI MAN CHAN")} />
-                </Grid>
-                <Grid item xs={12} sm={2}>
-                  <TextField label="Age" {...useFormInput("42")} />
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                  <TextField label="Dob" {...useFormInput("01/01/1977")} />
-                </Grid>
-                <Grid item xs={12} sm={1}>
-                  <TextField label="Sex" {...useFormInput("M")} />
-                </Grid>
-                <Grid item xs={12} sm={1}>
-                  <TextField label="Ward" {...useFormInput("4B")} />
-                </Grid>
-                <Grid item xs={12} sm={1}>
-                  <TextField label="Bed" {...useFormInput("34")} />
-                </Grid>
-                <Grid item xs={12} sm={2}>
-                  <TextField label="Speciality" {...useFormInput("CNT")} />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                  <TextField label="MRN" {...useFormInput("N/A")} />
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-          <Box m={4}>
-            <Grid container spacing={4}>
-              <Grid
-                item
-                sm
-                container
-                spacing={2}
-                justify="flex-start"
-                alignItems="center"
-              >
-                <Grid item>
-                  <FormLabel>Specimen No. / Lab #</FormLabel>
-                </Grid>
-                <Grid item>
-                  <TextField
-                    inputProps={{ "aria-label": "bare" }}
-                    {...useFormInput("XXXXXX")}
-                  />
-                </Grid>
-              </Grid>
-              <Grid
-                item
-                sm
-                container
-                spacing={2}
-                justify="flex-end"
-                alignItems="center"
-              >
-                <Grid item>
-                  <FormLabel>Pay Code</FormLabel>
-                </Grid>
-                <Grid item sm={2}>
-                  <TextField
-                    inputProps={{ "aria-label": "bare" }}
-                    {...useFormInput("X")}
-                  />
-                </Grid>
-              </Grid>
-            </Grid>
-          </Box>
-          <Card>
-            <CardContent>
-              <Tabs
-                value={tabValue}
-                onChange={(_, value) => setTabValue(value)}
-              >
-                <Tab label="Specimen Detail" />
-                <Tab label="Specimen Other Details" />
-                <Tab label="Order Info" />
-              </Tabs>
-              <Box padding={2}>
-                {tabValue === 0 && (
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={3}>
-                      <TextField label="Specimen" value={"XXXXXX"} />
-                    </Grid>
-                    <Grid item xs={12} sm={3}>
-                      <TextField label="Type Detail" value={"Testing"} />
-                    </Grid>
-                    <Grid item xs={12} sm={1}>
-                      <TextField label="Status" value={"P"} />
-                    </Grid>
-                    <Grid item xs={12} sm={1}>
-                      <TextField label="Marker" value={"TX"} />
-                    </Grid>
+    <Formik
+      initialValues={{
+        case: "MH1238474589634",
+        hkid: "A123456(3)",
+        name: "DAI MAN CHAN",
+        age: "42",
+        dob: "01/01/1977",
+        sex: "M",
+        ward: "4B",
+        bed: "34",
+        speciality: "CNT",
+        mrn: "N/A"
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(false);
+          alert(JSON.stringify(values, null, 2));
+        }, 500);
+      }}
+      render={() => (
+        <Form className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Card>
+              <CardContent>
+                <Typography>Patient Information</Typography>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={3}>
+                    <Field name="case" label="Case" component={TextField} />
                   </Grid>
-                )}
-              </Box>
-            </CardContent>
-          </Card>
-        </Paper>
-        <MadeWithLove />
-      </main>
-    </React.Fragment>
+                  <Grid item xs={12} sm={3}>
+                    <Field name="hkid" label="HKID" component={TextField} />
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <Field name="name" label="Name" component={TextField} />
+                  </Grid>
+                  <Grid item xs={12} sm={2}>
+                    <Field name="age" label="Age" component={TextField} />
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <Field name="dob" label="Dob" component={TextField} />
+                  </Grid>
+                  <Grid item xs={12} sm={1}>
+                    <Field name="sex" label="Sex" component={TextField} />
+                  </Grid>
+                  <Grid item xs={12} sm={1}>
+                    <Field name="ward" label="Ward" component={TextField} />
+                  </Grid>
+                  <Grid item xs={12} sm={1}>
+                    <Field name="bed" label="Bed" component={TextField} />
+                  </Grid>
+                  <Grid item xs={12} sm={2}>
+                    <Field
+                      name="speciality"
+                      label="Speciality"
+                      component={TextField}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <Field name="mrn" label="MRN" component={TextField} />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+            <Box m={4}>
+              <Grid container spacing={4}>
+                <Grid
+                  item
+                  sm
+                  container
+                  spacing={2}
+                  justify="flex-start"
+                  alignItems="center"
+                >
+                  <Grid item>
+                    <FormLabel>Specimen No. / Lab #</FormLabel>
+                  </Grid>
+                  <Grid item>
+                    <Field
+                      name="specimenNo"
+                      component={TextField}
+                      inputProps={{ "aria-label": "bare" }}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid
+                  item
+                  sm
+                  container
+                  spacing={2}
+                  justify="flex-end"
+                  alignItems="center"
+                >
+                  <Grid item>
+                    <FormLabel>Pay Code</FormLabel>
+                  </Grid>
+                  <Grid item sm={2}>
+                    <Field
+                      name="payCode"
+                      component={TextField}
+                      inputProps={{ "aria-label": "bare" }}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Box>
+            <Card>
+              <CardContent>
+                <Tabs
+                  value={tabValue}
+                  onChange={(_, value) => setTabValue(value)}
+                >
+                  <Tab label="Specimen Detail" />
+                  <Tab label="Specimen Other Details" />
+                  <Tab label="Order Info" />
+                </Tabs>
+                <Box padding={2}>
+                  {tabValue === 0 && (
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} sm={3}>
+                        <Field
+                          name="specimen"
+                          label="Specimen"
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={3}>
+                        <Field
+                          name="typeDetail"
+                          label="Type Detail"
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={1}>
+                        <Field
+                          name="status"
+                          label="Status"
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={1}>
+                        <Field
+                          name="marker"
+                          label="Marker"
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={12}>
+                        <Field
+                          name="site"
+                          label="Site"
+                          multiline
+                          rows="2"
+                          rowsMax="4"
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={12}>
+                        <Field
+                          name="preparation"
+                          label="Preparation"
+                          multiline
+                          rows="2"
+                          rowsMax="4"
+                          component={TextField}
+                        />
+                      </Grid>
+                    </Grid>
+                  )}
+                </Box>
+              </CardContent>
+            </Card>
+          </Paper>
+        </Form>
+      )}
+    />
   );
 }
